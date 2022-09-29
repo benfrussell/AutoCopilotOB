662ad8f8811dd848d98d17c40c31af8be2d6450d
benfrussell <benfrussell@gmail.com>
2022-09-29 13:52:26 -0400

    Restructure project for tests

---
+++ tests/test_zmqio.py
@@ -0,0 +1,9 @@
+from unittest import TestCase, main
+from unittest.mock import Mock
+
+class TestIPCThreadStartStop(TestCase):
+    def create_ipc_thread(self):
+        pass
+
+if __name__ == '__main__':
+    main()
